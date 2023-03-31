c8b2fee40e9011ae63f6f31c0737597ef3e4801e
Katerina Anurieva <113555695+katushhiaa@users.noreply.github.com>
2023-03-31 14:08:39 +0300

    Add files via upload

---
+++ Task01/Main.java
@@ -0,0 +1,17 @@
+
+/**
+ * Print command-line parameters.
+ */
+public class Main {
+
+    /**
+     * Program entry point.
+     *
+     * @param args command-line parameters list
+     */
+    public static void main(String[] args) {
+        for (String s : args) {
+            System.out.println(s);
+        }
+    }
+}
