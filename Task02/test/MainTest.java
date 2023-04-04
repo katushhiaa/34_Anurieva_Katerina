f96e09497b9c636247dd1846b266e5f819debe60
Katerina Anurieva <113555695+katushhiaa@users.noreply.github.com>
2023-04-04 17:44:09 +0300

    Add files via upload

--- Task02/test/MainTest.java
+++ Task02/test/MainTest.java
@@ -1,71 +1,64 @@
+package ex01;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import java.io.IOException;
-import ex01.Calc;
 import static org.junit.Assert.fail;
 
 
- /**
- * The class Main test
- */ 
+/*
+ * The class Calc test
+ */
 public class MainTest {
 
     private static final double DELTA = 0.001;
 
     @Test
-
-/** 
- * Test calculate sin average
- */
-    public void testCalculateSinAverage() { 
-
+    /*
+     * Test calculate sin average
+     */
+    public void testCalculateSinAverage() {
         Calc calc = new Calc();
         double result = calc.calculateSinAverage(90, 90, 90, 90);
         assertEquals(1000, result, DELTA);
     }
 
     @Test
-
-/** 
- * Test calculate ones count
- */
-    public void testCalculateOnesCount() { 
-
+    /*
+     * Test calculate ones count
+     */
+    public void testCalculateOnesCount() {
         Calc calc = new Calc();
         int result = calc.calculateOnesCount(90, 90, 90, 90);
         assertEquals(6, result);
     }
 
     @Test
-
-/** 
- * Test init
- */
-    public void testInit() { 
-
+    /*
+     * Test init
+     */
+    public void testInit() {
         Calc calc = new Calc();
         calc.init(90, 90, 90, 90);
-        double result = calc.getResult();
-        assertEquals(1000, result, DELTA);
+        Item2d result = calc.getResult();
+        assertEquals(new Item2d(90,90,90, 90), result);
     }
 
     @Test
-
-/** 
- * Test save and restore
- */
-    public void testSaveAndRestore() { 
-
+    /**
+     * Test save and restore
+     */
+    public void testSaveAndRestore() throws Exception {
         Calc calc = new Calc();
         calc.init(90, 90, 90, 90);
         try {
             calc.save();
-            calc.init(91, 91, 91, 91);
+            calc.init(90, 90, 90, 90);
             calc.restore();
         } catch (IOException | ClassNotFoundException e) {
             fail("Exception should not have been thrown");
         }
-        double result = calc.getResult();
-        assertEquals(1000, result, DELTA);
+        Item2d result = calc.getResult();
+        assertEquals(new Item2d(90,90,90, 90), result);
     }
 }
