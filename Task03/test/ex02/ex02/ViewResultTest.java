d42f0eb8aa9ccd6cd5045170abb82a4f391931b0
Katerina Anurieva <113555695+katushhiaa@users.noreply.github.com>
2023-04-04 19:56:32 +0300

    Add files via upload

---
+++ Task03/test/ex02/ex02/ViewResultTest.java
@@ -0,0 +1,85 @@
+package ex02;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.IOException;
+import java.util.ArrayList;
+import ex01.Item2d;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.fail;
+import org.junit.BeforeClass;
+/**
+ * The class View result test
+ */
+public class ViewResultTest {
+
+    ViewResult viewResult;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+
+    /**
+     * Sets the up
+     */
+    public void setUp() {
+
+        viewResult = new ViewResult(2);
+        viewResult.viewInit();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test calculate ones count
+     */
+    @Test
+    public void testCalculateOnesCount() {
+
+        int expected1 = Integer.bitCount((int) viewResult.calculateOnesCount(viewResult.getItems().get(0).getArg1(), viewResult.getItems().get(0).getArg2(), viewResult.getItems().get(0).getArg3(), viewResult.getItems().get(0).getArg4()));
+        int expected2 = Integer.bitCount((int) viewResult.calculateOnesCount(viewResult.getItems().get(1).getArg1(), viewResult.getItems().get(1).getArg2(), viewResult.getItems().get(1).getArg3(), viewResult.getItems().get(1).getArg4()));
+        assertEquals(expected1, viewResult.calculateOnesCount(viewResult.getItems().get(0).getArg1(), viewResult.getItems().get(0).getArg2(), viewResult.getItems().get(0).getArg3(), viewResult.getItems().get(0).getArg4()));
+        assertEquals(expected2, viewResult.calculateOnesCount(viewResult.getItems().get(1).getArg1(), viewResult.getItems().get(1).getArg2(), viewResult.getItems().get(1).getArg3(), viewResult.getItems().get(1).getArg4()));
+    }
+
+    @Test
+
+    /**
+     *
+     * Test view save
+     *
+     */
+    public void testRestore() {
+
+        try {
+            viewResult.viewSave();
+        } catch (IOException e) {
+        }
+        ArrayList<Item2d> items = viewResult.getItems();
+        ViewResult viewResult1 = new ViewResult(items.size());
+        try {
+            viewResult1.viewRestore();
+        } catch (Exception e) {
+        }
+        ArrayList<Item2d> items1 = viewResult1.getItems();
+        assertEquals(items.size(), items1.size());
+        for (int i = 0; i < items.size(); i++) {
+            assertEquals(items.get(i).getArg1(), items1.get(i).getArg1(), 0.1);
+            assertEquals(items.get(i).getArg2(), items1.get(i).getArg2(), 0.1);
+            assertEquals(items.get(i).getArg3(), items1.get(i).getArg3(), 0.1);
+            assertEquals(items.get(i).getArg4(), items
+                    .get(i).getArg4(), 0.1);
+        }
+    }
+
+}
